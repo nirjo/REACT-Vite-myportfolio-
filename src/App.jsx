4fe0a362cb6bcdf2407cbf172c8ae4f3f677696d
Nirmalraj <nirmalraj.joseph@twilightsoftwares.com>
2023-03-22 11:21:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,8 @@ const App = () => {
       <Hireme />
       <Contact />
       <footer className="p-3 text-center">
-        <h6 className="mb-3">JOHN ALEX</h6>
-        <p>codeaprogram © All CopyRights Reserved 2022</p>
+        <h6 className="mb-3">NIRMALRAJ JOSEPH</h6>
+        <p>cuteprogram © All CopyRights Reserved 2023</p>
       </footer>
     </div>
   );
